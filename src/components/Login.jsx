c26bee5d4067ff3ea63b86273f5dcf92a2910319
danielftrii <daienlsocial@gmail.com>
2025-06-29 10:27:07 +0800

    Adjusted font colour login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -120,10 +120,10 @@ const Login = () => {
 
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className={`mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-50`}>
+          <h2 className={`mt-6 text-center text-3xl font-extrabold text-gray-900`}>
             Sign in to your account
           </h2>
-          <p className={`mt-2 text-center text-sm text-gray-600 dark:text-gray-300`}>
+          <p className={`mt-2 text-center text-sm text-gray-600 dark:text-gray-500`}>
             Or{' '}
             <Link to="/register" className="font-medium text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300">
               create a new account
